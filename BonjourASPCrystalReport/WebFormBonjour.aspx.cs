5bf3192edc63b24fb7069df98fa1df1bcd9e7097
ESSARRAJ <essarraj.fouad@gmail.com>
2018-04-03 12:18:44 +0200

    init comit

---
+++ BonjourASPCrystalReport/WebFormBonjour.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace BonjourASPCrystalReport
+{
+    public partial class WebFormBonjour : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Bonjour bonjour = new Bonjour();
+            CrystalReportViewer1.ReportSource = null;
+            CrystalReportViewer1.ReportSource = bonjour;
+            CrystalReportViewer1.DataBind();
+        }
+    }
+}
